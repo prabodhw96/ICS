e01c084cb8606aef48990a2e9684e2b2982343d5
prabodhw96 <prabodhwankhede1996@gmail.com>
2019-01-11 03:25:50 +0530

    Add RSA

---
+++ RSA.py
@@ -0,0 +1,40 @@
+from math import sqrt
+def prime(pr):
+    for i in range(2, int(sqrt(pr))):
+        if pr%i == 0:
+            return False
+    return True
+
+def gcd(x, y):
+    while(y):
+        x, y = y, x % y 
+    return x
+
+import sys
+def calc_key(phi):
+    t = phi+1
+    for i in range(2, phi):
+        if prime(i) and t%i == 0:
+            e = i
+            d = int(t/e)
+            return e, d
+        if i == phi-1:
+            sys.exit("Can't compute keys for existing prime nos.")
+
+p = 131
+q = 139
+n = p*q
+phi = (p-1)*(q-1)
+e, d = calc_key(phi)
+msg = 18209
+encrypt_msg = pow(msg, e, n)
+decrypt_msg = pow(encrypt_msg, d, n)
+
+print("Original Message: ", msg)
+print("Encryption Key: ", e)
+print("Decryption Key: ", d)
+print("Encrypted Message: ", encrypt_msg)
+print("Decrypted Message: ", decrypt_msg)
+
+# Note: msg < n
+# As n increases, msg << n
